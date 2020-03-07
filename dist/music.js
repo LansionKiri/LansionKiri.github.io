7ceb0db904b9bc729876c951517246c14863ccad
LansionKiri <1813071712@qq.com>
2020-03-08 00:07:10 +0800

    Site updated: 2020-03-08 00:07:09

--- dist/music.js
+++ dist/music.js
@@ -1,12 +1,12 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     audio: [
 	{
-        name: "演员",
+        name: "刚刚好",
         artist: '朴树',
-        url: 'http://m10.music.126.net/20200308000137/c73d0d86c2ac3207a93e3dd9295a9385/ymusic/e7c5/84f9/897e/a897fda63f7e9f788eac7abbc0bf8602.mp3',
+        url: 'http://m10.music.126.net/20200308003119/3c9ceb3163411333adc661c19b549cac/ymusic/632f/4aed/a5ac/aa14f1a96651ac1b45a24663b1ec108e.mp3',
         cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
       },
     ]
