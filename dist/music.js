05082f111dc198e96a3fa54891483bce0cf43426
LansionKiri <1813071712@qq.com>
2020-03-09 17:49:39 +0800

    Site updated: 2020-03-09 17:49:39

--- dist/music.js
+++ dist/music.js
@@ -10,7 +10,7 @@ const ap = new APlayer({
         name: "演员",
         artist: '薛之谦',
         url: 'https://m701.music.126.net/20200309161103/4fc0f7101027f32b629f8083c7ba6e9d/jdyyaac/5358/0609/5409/d2c0282f3c1c160c13f9cc4684aa9853.m4a',
-        cover: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2755361489,642864069&fm=26&gp=0.jpg',
+        cover: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2268073604,4172593995&fm=26&gp=0.jpg',
       }
     ]
 });
