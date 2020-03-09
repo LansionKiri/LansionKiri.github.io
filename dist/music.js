b69a1b0cc1f5d92ffbf2b5db72510f1216842a2a
LansionKiri <1813071712@qq.com>
2020-03-09 18:36:50 +0800

    Site updated: 2020-03-09 18:36:49

--- dist/music.js
+++ dist/music.js
@@ -2,6 +2,14 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: true,
+	loop: 'all',
+    order: 'random',
+    preload: 'auto',
+	volume: 0.7,            <!-- 默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效-->
+    mutex: true,            <!-- 互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器 -->
+    listFolded: false,      <!-- 列表默认折叠， false表示不折叠-->
+    listMaxHeight: '120px', <!-- 列表最大高度，超出此高度将有滚动条-->
+    lrcType: 0,             <!-- 歌词类型，0表示不显示歌词-->
     audio: [
       {
         name: "演员",
