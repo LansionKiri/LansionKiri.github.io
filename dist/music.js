83fead4d6fd8778b1de13a1a180a858529cb3fca
LansionKiri <1813071712@qq.com>
2020-03-11 07:59:03 +0800

    Site updated: 2020-03-11 07:59:03

--- dist/music.js
+++ dist/music.js
@@ -3,9 +3,9 @@ const ap = new APlayer({
     fixed: true,
     autoplay: true,
 	loop: 'all',
-    order: 'random',
+    order: 'list',
     preload: 'auto',
-	// volume: 0.7,            <!-- 默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效-->
+	volume: 0.7,            <!-- 默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效-->
     mutex: true,            <!-- 互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器 -->
     listFolded: false,      <!-- 列表默认折叠， false表示不折叠-->
     listMaxHeight: '120px', <!-- 列表最大高度，超出此高度将有滚动条-->
